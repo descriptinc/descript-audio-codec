e0150d3a42a1fbe252055ce94cc60c8c83c81a39
Ishaan Kumar <ishaan@descript.com>
2023-06-13 20:42:24 +0000

    add hubconf support

--- dac/utils/__init__.py
+++ dac/utils/__init__.py
@@ -9,7 +9,7 @@ DAC = dac.model.DAC
 Accelerator = ml.Accelerator
 
 
-def ensure_default_model(tag: str = dac.__model_version__):
+def ensure_default_model(tag: str = dac.__model_version__, model_path: str = ""):
     """
     Function that downloads the weights file from URL if a local cache is not
     found.
@@ -18,9 +18,14 @@ def ensure_default_model(tag: str = dac.__model_version__):
         tag (str): The tag of the model to download.
     """
     download_link = f"https://github.com/descriptinc/descript-audio-codec/releases/download/{tag}/weights.pth"
-    local_path = Path.home() / ".cache" / "descript" / tag / "dac" / f"weights.pth"
-    if not local_path.exists():
-        local_path.parent.mkdir(parents=True, exist_ok=True)
+
+    # Set the default model path
+    if not model_path:
+        model_path = Path.home() / ".cache" / "descript" / tag / "dac" / "weights.pth"
+
+    # Check if the model exists
+    if not model_path.exists():
+        model_path.parent.mkdir(parents=True, exist_ok=True)
 
         # Download the model
         import requests
@@ -31,18 +36,17 @@ def ensure_default_model(tag: str = dac.__model_version__):
             raise ValueError(
                 f"Could not download model. Received response code {response.status_code}"
             )
-        local_path.write_bytes(response.content)
+        model_path.write_bytes(response.content)
 
     # return the path required by audiotools to load the model
-    return local_path.parent.parent
+    return model_path.parent.parent
 
 
 def load_model(
     tag: str,
     load_path: str = "",
 ):
-    if not load_path:
-        load_path = ensure_default_model(tag)
+    load_path = ensure_default_model(tag, load_path)
     kwargs = {
         "folder": load_path,
         "map_location": "cpu",
---
+++ hubconf.py
@@ -0,0 +1,47 @@
+dependencies = [
+    "argbind>=0.3.7",
+    "audiotools @ git+https://github.com/descriptinc/audiotools.git@0.7.0",
+    "einops",
+    "numpy",
+    "torch",
+    "torchaudio",
+    "tqdm",
+]
+from pathlib import Path
+
+import torch
+
+from dac import DAC
+from dac import __model_version__
+from dac import load_model as _load_model
+
+
+def load_model(pretrained: bool = False, tag: str = __model_version__, **kwargs):
+    """Load model
+
+    Parameters
+    ----------
+    pretrained : bool, optional
+        If `True` use the pretrained model weights to initialize
+        the model, by default False.
+    tag : str, optional
+        Model tag, by default 0.0.1
+    **kwargs
+        Other keyword arguments used to initialize the model.
+        These are ignored when `pretrained` is `True`. See
+        `dac.model.DAC` for more details.
+
+    Returns
+    -------
+    dac.DAC
+        Model object
+    """
+    if pretrained:
+        model_path = (
+            Path(torch.hub.get_dir()) / "descript" / tag / "dac" / "weights.pth"
+        )
+        model = _load_model(tag, load_path=model_path)
+    else:
+        model = DAC(**kwargs)
+
+    return model
