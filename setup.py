2021f0ad16d8ed7146862fae40dcee7a5b1e2435
Hugo Flores Garcia <hf01049@georgiasouthern.edu>
2023-07-17 10:58:45 -0500

    bump audiotools version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     keywords=["audio", "compression", "machine learning"],
     install_requires=[
         "argbind>=0.3.7",
-        "descript-audiotools==0.7.1",
+        "descript-audiotools @ git+https://github.com/descriptinc/audiotools.git@0.7.2",
         "einops",
         "numpy",
         "torch",
