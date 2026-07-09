f12cfe35c773d4a51fb85755237ef9c64688e10e
xingzheh <xingzhe@descript.com>
2026-07-09 07:00:34 +0000

    Make decoder causal again

--- dac/nn/layers.py
+++ dac/nn/layers.py
@@ -44,17 +44,15 @@ class PaddedConvTranspose1d(nn.Module):
         kwargs.pop('padding', None)
         causal = kwargs.pop('causal', False)
         padding = (kwargs.get('kernel_size', 1) - 1) * kwargs.get('dilation', 1) - kwargs.get('stride', 1) + 1
-        self.padding = (
-            padding if causal else padding // 2,
-            0 if causal else padding // 2
-        )
+        self.trim_left = 0 if causal else padding // 2
+        self.trim_right = padding if causal else padding // 2
         self.conv = weight_norm(nn.ConvTranspose1d(*args, **kwargs))
         
     def forward(self, x):
-        # Run the transposed convolution
         x = self.conv(x)
-        # Drop the last samples
-        return x[..., self.padding[0]:-self.padding[1]] if self.padding[1] > 0 else x[..., self.padding[0]:]
+        if self.trim_right > 0:
+            return x[..., self.trim_left:-self.trim_right]
+        return x[..., self.trim_left:]
 
 def WNConv1d(*args, **kwargs):
     return PaddedConv1d(*args, **kwargs)
